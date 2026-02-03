6b33e1d608326d8dedb0a429a98fb26f5059d38f
deepi30112007 <deepi30112007@gmail.com>
2026-02-03 17:39:15 +0530

    Add files via upload

---
+++ salary.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class salary{
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    int salary=sc.nextInt(); 
+    
+    if(salary>=50000){
+      System.out.println("bonus= "+salary*0.10);
+    }
+    else if(salary>=30000){
+      System.out.println("bonus= "+salary*0.07);
+    }
+    else{
+      System.out.println("bonus= "+salary*0.05);
+    }
+  }
+}
